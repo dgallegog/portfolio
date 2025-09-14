ce544a6aa641679c381afa9dd65639283a539835
David Gallego <dgallegog@MacBook-Air-de-David.local>
2025-09-15 06:49:06 +0700

    added icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} davidgallego.dev | All rights reserved.
+        &copy; {new Date().getFullYear()} dgallego.dev | All rights reserved.
       </p>
       <a
         href="#hero"
